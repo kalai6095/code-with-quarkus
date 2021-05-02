0ec6285a487357cc0ce518f99b86d0e8947abe99
Kalaiselvan A <kalai6095@gmail.com>
2021-05-02 19:47:38 +0000

    adding docker file

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,8 +1,10 @@
 package org.acme;
 
+import javax.ws.rs.POST;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello-resteasy")
@@ -12,5 +14,12 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String addhello(String body) {
+        return body;
     }
 }
